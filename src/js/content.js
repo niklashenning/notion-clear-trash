9b6d9fad6d6bfb05e940f4a74c13dba024b69882
Niklas Henning <niklashenningbusiness@gmail.com>
2024-10-10 00:32:42 +0200

    Implement getting trashed pages

--- src/js/content.js
+++ src/js/content.js
@@ -2,6 +2,9 @@
 window.onload = function() {
     getSpaceId().then(spaceId => { 
         console.log(spaceId);
+        getTrashedPages(spaceId).then(trashedPages => { 
+            console.log(trashedPages);
+        });
     });
 };
 
@@ -9,6 +12,10 @@ window.onload = function() {
 async function getSpaceId() {
     let apiEndpoint = "https://www.notion.so/api/v3/loadUserContent";
     let options = {
+        headers: {
+            "cache-control": "no-cache",
+            "content-type": "application/json",
+        },
         method: "POST",
         body: "{}"
     }
@@ -17,3 +24,44 @@ async function getSpaceId() {
     let json = await response.json();
     return Object.keys(json.recordMap.space)[0];
 };
+
+
+async function getTrashedPages(spaceId) {
+    let apiEndpoint = "https://www.notion.so/api/v3/search";
+    let options = {
+        headers: {
+            "cache-control": "no-cache",
+            "content-type": "application/json",
+        },
+        method: "POST",
+        body: `
+            {
+                "type": "BlocksInSpace",
+                "spaceId": "` + spaceId + `",
+                "limit": 20,
+                "filters": {
+                    "isDeletedOnly": true,
+                    "excludeTemplates": false,
+                    "navigableBlockContentOnly": false,
+                    "requireEditPermissions": false,
+                    "includePublicPagesWithoutExplicitAccess": false,
+                    "ancestors": [],
+                    "createdBy": [],
+                    "editedBy": [],
+                    "lastEditedTime": {},
+                    "createdTime": {},
+                    "inTeams": []
+                },
+                "sort": {
+                    "field": "relevance"
+                },
+                "source": "trash",
+                "searchExperimentOverrides": {}
+            }
+        `
+    }
+
+    let response = await fetch(apiEndpoint, options);
+    let json = await response.json();
+    return json.results.map(object => object.id);
+}
