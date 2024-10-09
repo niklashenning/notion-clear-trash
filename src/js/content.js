2310d386b1d77eca165b32e815f702bcbd532470
Niklas Henning <niklashenningbusiness@gmail.com>
2024-10-09 23:47:19 +0200

    Implement getting space id

--- src/js/content.js
+++ src/js/content.js
@@ -1,4 +1,19 @@
 
 window.onload = function() {
-    console.log("content script");
+    getSpaceId().then(spaceId => { 
+        console.log(spaceId);
+    });
+};
+
+
+async function getSpaceId() {
+    let apiEndpoint = "https://www.notion.so/api/v3/loadUserContent";
+    let options = {
+        method: "POST",
+        body: "{}"
+    }
+
+    let response = await fetch(apiEndpoint, options);
+    let json = await response.json();
+    return Object.keys(json.recordMap.space)[0];
 };
